353d6f6cd1ab9f0ed9e3aae33f4ae83514a9d9cd
Jeronimo Repetto <jeronimorepetto@gmail.com>
2022-05-17 19:06:41 +0200

    Finalizado el socket server

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,21 +1,48 @@
 const { io } = require('../index.js');
+const Band = require('../models/band');
+const Bands = require('../models/bands.js');
+const bands = new Bands();
+
+bands.addBand(new Band('Viejo Miseria'));
+bands.addBand(new Band('Queen'));
+bands.addBand(new Band('The Beatles'));
+bands.addBand(new Band('Metalica'));
+
 
 // Mensajes de Socket
 io.on('connection', client => {
     console.log('Cliente conectado');
-    client.on('disconnect', () => { 
+
+    client.emit('active-bands', bands.getBands());
+
+    client.on('disconnect', () => {
         console.log('Cliente desconectado');
     });
 
-    client.on('mensaje',( payload )=> {
-        console.log("Mensaje!!", payload );
+    // client.on('mensaje', (payload) => {
+    //    console.log("Mensaje!!", payload);
+    //    io.emit('mensaje', { admin: 'Nuevo mensaje' });
+    //  });
 
-        io.emit( 'mensaje' , { admin: 'Nuevo mensaje'});
+    client.on('vote-band', (payload) => {
+        bands.voteBand(payload.id);
+        io.emit('active-band', bands.getBands());
     });
 
-    client.on('emitir-mensaje', ( payload ) =>{
-        //console.log(payload);
-        //io.emit('nuevo-mensaje', payload); // Emite a todos!
-        client.brodcast.emit('nuevo-mensaje', payload);//emite a todos menos al que lo emitio
+    client.on('add-band', (payload) => {
+        bands.addBand(new Band.addBand(payload.name));
+        io.emit('active-band', bands.getBands());
     });
-  });
+
+
+    client.on('delete-band', (payload) => {
+        bands.deleteBand(payload.id);
+        io.emit('active-band', bands.getBands());
+    });
+
+    // client.on('emitir-mensaje', (payload) => {
+    //console.log(payload);
+    //io.emit('nuevo-mensaje', payload); // Emite a todos!
+    //    client.brodcast.emit('nuevo-mensaje', payload);//emite a todos menos al que lo emitio
+    // });
+});
